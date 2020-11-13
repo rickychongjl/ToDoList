8480d823b99e4fd4e59960deab77d6db5f3e9b2c
Ricky Chong <rickychongjl@gmail.com>
2020-11-13 15:55:14 +1000

    Add project files.

---
+++ ToDoList.Web/ClientApp/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { AuthModule } from '@auth0/auth0-angular';
+import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { HomeComponent } from './home/home.component';
+import { AtlasComponent } from './atlas/atlas/atlas.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    AtlasComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+    ]),
+    AuthModule.forRoot({
+      domain: 'dev-ui031igo.au.auth0.com',
+      clientId: 'I6PE1FxsZsy1l52V9AgTTKWDdnS0svXe'
+    })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
